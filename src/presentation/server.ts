6f7779a8be4254d3af6f9009028c6c8542cf4d57
CAS <alejo.hortua3@gmail.com>
2025-12-19 22:10:12 -0500

    Carga proyecto

---
+++ src/presentation/server.ts
@@ -0,0 +1,28 @@
+import { CronJob } from "cron";
+import { CronService } from "./cron/cron-service";
+import { CheckService } from "../domain/use-cases/checks/check-service";
+
+
+export class Server {
+
+    static start(){
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+
+                const url = `https://google.com`;
+
+                new CheckService(
+                    () => console.log(`${url} is ok.`),
+                    (error) => console.log(error)
+                ).execute(`${url}`);
+                // new CheckService().execute(`http://localhost:3000/`);
+            }
+        );
+
+    }
+
+
+}
